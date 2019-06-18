4dc22beb934e9e1594b3e9161b337f979653a301
RafSwiggers <rafswiggers@gmail.com>
2019-06-18 14:13:21 +0200

    attempt at making the block appear and reappear

--- script/javascript.js
+++ script/javascript.js
@@ -1,14 +1,21 @@
 const userBlock = document.getElementById('block');
+counter = 0;
 const food = document.getElementById('food');
 randompositionx = Math.floor(Math.random() * 25) * 2;
 randompositiony = Math.floor(Math.random() * 25) * 2;
 var posx = 0;
 var posy = 0;
+food.style.left = randompositionx + "vw";
+food.style.top = randompositiony + "vw";
 document.addEventListener('keydown', function(e) {
-    console.log(e.which);
+    console.log(counter);
+    if ((food.style.left === userBlock.style.left) && (food.style.top === userBlock.style.top)) {
+        counter++;
+        food.style.left = Math.floor(Math.random() * 25) * 2;
+        food.style.top = Math.floor(Math.random() * 25) * 2;
+    };
     if (e.which === 39) {
         posx += 2;
-        console.log(posx)
         userBlock.style.left = posx + "vw";
     } else if (e.which === 37) {
         posx -= 2;
@@ -20,6 +27,11 @@ document.addEventListener('keydown', function(e) {
         posy -= 2;
         userBlock.style.top = posy + "vw";
     }
+
 })
-food.style.left = randompositionx + "vw";
-food.style.top = randompositiony + "vw";
+
+
+document.addEventListener("change", function() {
+    console.log("works");
+
+});
