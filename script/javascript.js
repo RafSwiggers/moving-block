a5c24cbb4f40a6760e300fe8da6eaf1a23976f0a
RafSwiggers <rafswiggers@gmail.com>
2019-06-18 13:57:43 +0200

    add movement and second block that appears randomly

--- script/javascript.js
+++ script/javascript.js
@@ -1,20 +1,25 @@
 const userBlock = document.getElementById('block');
+const food = document.getElementById('food');
+randompositionx = Math.floor(Math.random() * 25) * 2;
+randompositiony = Math.floor(Math.random() * 25) * 2;
 var posx = 0;
 var posy = 0;
 document.addEventListener('keydown', function(e) {
     console.log(e.which);
     if (e.which === 39) {
-        posx += 5;
+        posx += 2;
         console.log(posx)
-        userBlock.style.left = posx + "px";
+        userBlock.style.left = posx + "vw";
     } else if (e.which === 37) {
-        posx -= 5;
-        userBlock.style.left = posx + "px";
+        posx -= 2;
+        userBlock.style.left = posx + "vw";
     } else if (e.which === 40) {
-        posy += 5;
-        userBlock.style.top = posy + "px";
+        posy += 2;
+        userBlock.style.top = posy + "vw";
     } else if (e.which === 38) {
-        posy -= 5;
-        userBlock.style.top = posy + "px";
+        posy -= 2;
+        userBlock.style.top = posy + "vw";
     }
 })
+food.style.left = randompositionx + "vw";
+food.style.top = randompositiony + "vw";
